605904c3860223756b8f486ed73fcd78ae0b8876
johnisawesome94 <lunde367@umn.edu>
2021-03-12 13:36:24 -0600

    trying to add angular material

---
+++ src/app/add-stock/add-stock.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { AddStockComponent } from "./add-stock.component";
+
+@NgModule({
+  imports: [],
+  declarations: [AddStockComponent]
+})
+export class AddStockModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { AddStockModule } from "./add-stock/add-stock.module";
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { MaterialModule } from "./material.module";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    AddStockModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    MaterialModule,
+    ReactiveFormsModule
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
