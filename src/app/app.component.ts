ab3539f6d977954f76c7081c4c04debc655019a6
johnisawesome94 <lunde367@umn.edu>
2021-03-12 15:35:28 -0600

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { OverlayContainer } from "@angular/cdk/overlay";
 import { Component, HostBinding, VERSION } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { MatIconRegistry } from "@angular/material/icon";
@@ -16,7 +17,8 @@ export class AppComponent {
 
   constructor(
     private dialog: MatDialog,
-    private iconRegistry: MatIconRegistry
+    private iconRegistry: MatIconRegistry,
+    private overlay: OverlayContainer
   ) {}
 
   ngOnInit(): void {
@@ -32,5 +34,10 @@ export class AppComponent {
 
   public darkModeToggle(event: MatSlideToggleChange) {
     this.className = event.checked ? "darkMode" : "";
+    if (event.checked) {
+      this.overlay.getContainerElement().classList.add(this.darkClassName);
+    } else {
+      this.overlay.getContainerElement().classList.remove(this.darkClassName);
+    }
   }
 }
