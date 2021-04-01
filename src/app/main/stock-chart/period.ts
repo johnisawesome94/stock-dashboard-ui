c0cdd4e3fca0b99209e67329361196ea48876293
johnisawesome94 <lunde367@umn.edu>
2021-04-01 16:08:42 -0500

    add period selector that does nothing so far.

---
+++ src/app/main/stock-chart/period.ts
@@ -0,0 +1,68 @@
+export enum Period {
+  OneDay = "1d",
+  OneWeek = "7d",
+  OneMonth = "1mo",
+  OneYear = "1y",
+  YearToDate = "ytd",
+  Max = "max"
+}
+
+export namespace Period {
+  export function getDisplayValue(period: Period): string {
+    switch (period) {
+      case Period.OneDay:
+        return "1 day";
+      case Period.OneWeek:
+        return "1 week";
+      case Period.OneMonth:
+        return "1 month";
+      case Period.OneYear:
+        return "1 year";
+      case Period.YearToDate:
+        return "Year to date";
+      case Period.Max:
+        return "Max";
+    }
+  }
+
+  export function getPeriodOptions(): PeriodOptions[] {
+    return [
+      {
+        intervalType: "Days",
+        interval: 1,
+        displayValue: Period.getDisplayValue(Period.OneDay)
+      },
+      {
+        intervalType: "Weeks",
+        interval: 1,
+        displayValue: Period.getDisplayValue(Period.OneWeek)
+      },
+      {
+        intervalType: "Months",
+        interval: 1,
+        displayValue: Period.getDisplayValue(Period.OneMonth)
+      },
+      {
+        intervalType: "Years",
+        interval: 1,
+        displayValue: Period.getDisplayValue(Period.OneYear)
+      } //,
+      // {
+      //   intervalType: '',
+      //   interval: 1,
+      //   displayValue: Period.getDisplayValue(Period.YearToDate)
+      // },
+      // {
+      //   intervalType: "Days",
+      //   interval: 1,
+      //   displayValue: Period.getDisplayValue(Period.Max)
+      // }
+    ];
+  }
+}
+
+export interface PeriodOptions {
+  intervalType: "Years" | "Months" | "Weeks" | "Days" | "Auto";
+  interval: number;
+  displayValue: string;
+}
